48d810586746c021c5115b5a341ad9ba0447a194
EduardoTA <EduardoTA@live.com>
2022-10-23 21:18:10 -0300

    Alterada a classe Player para utilizar modo assíncrono de MediaCodec

--- android/app/src/main/java/com/example/filterproject/MainActivity.java
+++ android/app/src/main/java/com/example/filterproject/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.filterproject;
 
-import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,8 +13,6 @@ import android.media.MediaExtractor;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Environment;
-import android.os.ParcelFileDescriptor;
-import android.provider.MediaStore;
 import android.util.Log;
 import android.widget.TextView;
 import android.view.View.OnClickListener;
@@ -27,17 +24,12 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.io.DataInputStream;
 import java.io.File;
-import java.io.FileDescriptor;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.ShortBuffer;
-import java.util.Arrays;
-
-import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -85,100 +77,10 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void playWav(Uri uri) {
-        mPlayer.initialize(uri, getApplicationContext());
-        mPlayer.start();
-    }
-
-    public void playWav(File file) throws IOException {
-        int minBufferSize = AudioTrack.getMinBufferSize(48000, AudioFormat.CHANNEL_OUT_MONO, AudioFormat.ENCODING_PCM_FLOAT);
-        int bufferSize = 32768;
-        int bufferSize2 = bufferSize / 2;
-        int bufferSize4 = bufferSize / 4;
-        AudioTrack at = new AudioTrack(AudioManager.STREAM_MUSIC, 48000, AudioFormat.CHANNEL_OUT_MONO, AudioFormat.ENCODING_PCM_FLOAT, minBufferSize, AudioTrack.MODE_STREAM);
-
-        MediaExtractor me = new MediaExtractor();
-        me.setDataSource(file.getPath());
-        me.selectTrack(0);
-
-
-        int i;
-
-        byte[] s = new byte[bufferSize];
         try {
-            FileInputStream fin = new FileInputStream(file);
-            Log.d("nome2", file.getName());
-            DataInputStream dis = new DataInputStream(fin);
-            at.play();
-            double[] audioFloats = new double[bufferSize4];
-            float[] audioFloats2 = new float[bufferSize4];
-            float float_amp[] = new float[bufferSize4];
-            for (int h = 0; h < float_amp.length; h++) {
-                float_amp[h] = 0.0f;
-            }
-            //float x[] = new float[20];
-            //float h[]= {-0.0978f, 0.0877f, 0.0729f, 0.0698f, 0.0726f, 0.0780f, 0.0843f, 0.0897f, 0.0938f, 0.0961f, 0.0961f, 0.0938f, 0.0897f, 0.0843f, 0.0780f, 0.0726f, 0.0698f, 0.0729f, 0.0877f, -0.0978f};
-            double[] a = {1.000000000000, -9.39331398000000, 40.0758751800000, -102.252093950000, 172.765289700000, -201.969268100000, 165.442085090000, -93.7680344200000, 35.1938395300000, -7.89982228000000, 0.805444390000000};
-            double[] b = {0.00590141000000000, -0.0439812000000000, 0.140436120000000, -0.239946760000000, 0.205003030000000, 0, -0.205003030000000, 0.239946760000000, -0.140436120000000, 0.0439812000000000, -0.00590141000000000};
-            IIRFilter filter = new IIRFilter(a, b);
-            /*
-            while ((i = dis.read(s, 0, bufferSize)) > -1) {
-
-                ShortBuffer sbuf =
-                        ByteBuffer.wrap(s).order(ByteOrder.LITTLE_ENDIAN).asShortBuffer();
-                short[] audioShorts = new short[sbuf.capacity()];
-                sbuf.get(audioShorts);
-
-                for (int t = 0; t < bufferSize4; t++) {
-                    audioFloats[t] = ((double) audioShorts[2 * t]) / 0x8000;
-                }
-
-                filter.process(audioFloats, float_amp);
-
-                Log.i("Float_Amp", Arrays.toString(audioFloats));
-                //Log.v("float_amp",String.valueOf(float_amp[5]));
-                //Log.i("audioFloats", Arrays.toString(audioFloats));
-                //Log.i("Float_Amp", Arrays.toString(float_amp));
-                at.write(float_amp, 0, bufferSize4, AudioTrack.WRITE_BLOCKING);
-            }
-             */
-            float[] float_amp2 = new float[bufferSize4];
-            ByteBuffer bf = ByteBuffer.allocate(32768);
-            while ((i = (me.readSampleData(bf,0))) > -1){
-                //Log.d("tam amostra", String.valueOf(i));
-                ShortBuffer sbuf =
-                        bf.order(ByteOrder.LITTLE_ENDIAN).asShortBuffer();
-                short[] audioShorts = new short[sbuf.capacity()];
-                sbuf.get(audioShorts);
-
-                for (int t = 0; t < bufferSize4; t++) {
-                    audioFloats[t] = ((double) audioShorts[2 * t]) / 0x8000;
-                }
-
-                filter.process(audioFloats, float_amp, audioFloats.length);
-
-                //for (int t = 0; t < bufferSize4; t++) {
-                //    audioFloats2[t] = ((float) audioFloats[t]);
-                //}
-                //Log.i("Float_Amp", Arrays.toString(audioFloats));
-                //Log.v("float_amp",String.valueOf(float_amp[5]));
-                //Log.i("audioFloats", Arrays.toString(audioFloats));
-                //Log.i("Float_Amp", Arrays.toString(float_amp));
-                at.write(float_amp, 0, bufferSize4, AudioTrack.WRITE_BLOCKING);
-                me.advance();
-            }
-
-
-            at.stop();
-            at.release();
-            dis.close();
-            fin.close();
-
-        } catch (FileNotFoundException e) {
-            // TODO
-            Log.d("erro", e.toString());
-            e.printStackTrace();
+            mPlayer.initialize(uri, getApplicationContext());
+            mPlayer.start();
         } catch (IOException e) {
-            // TODO
             e.printStackTrace();
         }
     }
--- android/app/src/main/java/com/example/filterproject/Player.java
+++ android/app/src/main/java/com/example/filterproject/Player.java
@@ -4,20 +4,28 @@ import android.content.Context;
 import android.media.AudioFormat;
 import android.media.AudioManager;
 import android.media.AudioTrack;
+import android.media.MediaCodec;
+import android.media.MediaCodecList;
+import android.media.MediaExtractor;
+import android.media.MediaFormat;
 import android.net.Uri;
+import android.provider.MediaStore;
 import android.util.Log;
 
-import java.io.File;
+import androidx.annotation.NonNull;
+
+import java.io.FileDescriptor;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Phaser;
 
 public class Player {
-    Thread mThread = null;
-
-    private FileExtractor mFe = new FileExtractor();
     private Uri mUri;
-    private IIRFilter filter;
+    private FileDescriptor mFd;
+    private MediaExtractor mExtractor;
 
     float[] floatSamples;
     double[] doubleSamples;
@@ -25,74 +33,75 @@ public class Player {
     double[] doubleFiltered;
     short[] shortArray;
 
-    //float h[]= {-0.0978f, 0.0877f, 0.0729f, 0.0698f, 0.0726f, 0.0780f, 0.0843f, 0.0897f, 0.0938f, 0.0961f, 0.0961f, 0.0938f, 0.0897f, 0.0843f, 0.0780f, 0.0726f, 0.0698f, 0.0729f, 0.0877f, -0.0978f};
-    float h[]={-0.0930250949393319f, 0.0207057333964976f, 0.0291475759590533f, 0.0430501166124715f, 0.0601692514455458f, 0.0788068471187608f, 0.0968422736437664f, 0.112413676993475f, 0.123818015340460f, 0.129851026602985f, 0.129851026602985f, 0.123818015340460f, 0.112413676993475f, 0.0968422736437664f, 0.0788068471187608f, 0.0601692514455458f, 0.0430501166124715f, 0.0291475759590533f, 0.0207057333964976f, -0.0930250949393319f};
     double[] a = {1.000000000000, -9.39331398000000, 40.0758751800000, -102.252093950000, 172.765289700000, -201.969268100000, 165.442085090000, -93.7680344200000, 35.1938395300000, -7.89982228000000, 0.805444390000000};
     double[] b = {0.00590141000000000, -0.0439812000000000, 0.140436120000000, -0.239946760000000, 0.205003030000000, 0, -0.205003030000000, 0.239946760000000, -0.140436120000000, 0.0439812000000000, -0.00590141000000000};
+    private IIRFilter filter = new IIRFilter(a, b);
+
+    int numberOfBands = 5;
+    private IIRFilter[] filters;
+    Thread[] threads;
+    CountDownLatch startSignal;
+    CountDownLatch doneSignal;
+
+    Phaser startPhaser;
+    Phaser donePhaser;
 
     float[] x;
 
-    AudioTrack at;
+    AudioTrack mAt;
+    private MediaFormat mFormat;
+    private String mMime;
+    private MediaCodec mCodec;
 
-    public void initialize (Uri uri, Context context) {
-        if (mThread != null) {
-            boolean success = false;
-            for (int attempts = 0; attempts < 100 && success == false; attempts++) {
-                if (mFe.mState == FileExtractor.StateEnum.UNINITIALIZED
-                        && mThread.isAlive() == false) {
-                    success = true;
-                } else {
-                    mFe.stop();
-                    try {
-                        mThread.join(1000);
-                    } catch (InterruptedException e) {
-                    }
-                }
-            }
-            if (success == false) {
-                Log.e("Player", "Failed to stop Player, try again later");
-                return;
-            }
-            mThread = null;
-        }
-        boolean success = false;
-        for (int attempts = 0; attempts < 1000 && success == false; attempts++) {
-            try {
-                if (mFe.initialize(context.getApplicationContext(), uri) != null) {
-                    success = true;
-                }
-            } catch (IOException e) {
-                Log.e("Player", "Failed to find file, trying again");
-            }
-        }
-        if (success == false) {
-            Log.e("Player", "Failed to find file, try again later");
-            return;
-        }
+    static private MediaCodec createMediaCodec (MediaFormat format)
+            throws IOException, IllegalArgumentException {
+        assert format != null;
+        String codecName = new MediaCodecList(MediaCodecList.ALL_CODECS).findDecoderForFormat(format);
+        MediaCodec codec = null;
+        String mime = format.getString(MediaFormat.KEY_MIME);
+        //Log.d(, "try to create a codec mime="+mime+" codecName="+codecName);
+        if (codecName != null)
+            codec = MediaCodec.createByCodecName(codecName);
+        else if (mime != null)
+            codec = MediaCodec.createDecoderByType(mime);  // may be throw IllegalArgumentException
+        return codec;
+    }
 
-        int sampleRate = mFe.getSampleRate();
-        int numChannels = mFe.getNumChannels();
+    static private MediaExtractor createMediaExtractor (FileDescriptor fd) throws IOException {
+        MediaExtractor extractor = new MediaExtractor();
+        extractor.setDataSource(fd);
+        extractor.selectTrack(0);
 
-        if (numChannels > 2 || numChannels < 1) {
-            Log.e("Player", "Only supports mono or stereo");
-            return;
-        }
+        return extractor;
+    }
 
-        int channelConfig = numChannels == 1 ? AudioFormat.CHANNEL_OUT_MONO : AudioFormat.CHANNEL_OUT_STEREO;
+    public void initialize (Uri uri, Context context) throws IOException {
+        mFd = context.getContentResolver().openFileDescriptor(uri, "r").getFileDescriptor();
+
+        mExtractor = createMediaExtractor(mFd);
+
+        mFormat = mExtractor.getTrackFormat(0);
+
+        mMime = mFormat.getString(MediaFormat.KEY_MIME);
+
+        mCodec = createMediaCodec(mFormat);
+        mCodec.configure(mFormat, null, null, 0);
+        mCodec.getOutputFormat();
+
+        Integer sampleRate = mFormat.getInteger(MediaFormat.KEY_SAMPLE_RATE);
+        Integer numChannels = mFormat.getInteger(MediaFormat.KEY_CHANNEL_COUNT);
+
+        // int channelConfig = numChannels == 1 ? AudioFormat.CHANNEL_OUT_MONO : AudioFormat.CHANNEL_OUT_STEREO;
+        int channelConfig = AudioFormat.CHANNEL_OUT_MONO;
 
         int minBufferSize = AudioTrack.getMinBufferSize(sampleRate, channelConfig, AudioFormat.ENCODING_PCM_FLOAT);
         int bufferSize = 512;
-        at = new AudioTrack(AudioManager.STREAM_MUSIC,
+        mAt = new AudioTrack(AudioManager.STREAM_MUSIC,
                 sampleRate, channelConfig,
                 AudioFormat.ENCODING_PCM_FLOAT, minBufferSize,
                 AudioTrack.MODE_STREAM);
 
-
-        byte[] s = new byte[bufferSize];
-        at.play();
-        x = new float[20];
-        byte[] s_amp = new byte[s.length];
-        //float aux;
+        Log.v("gdf", "Sample Rate = "+sampleRate+", numChannels = "+numChannels);
 
         floatSamples = new float[0];
         doubleSamples = new double[0];
@@ -100,93 +109,77 @@ public class Player {
         doubleFiltered = new double[0];
         shortArray = new short[0];
 
-        filter = new IIRFilter(a, b);
+        mCodec.setCallback(new MediaCodec.Callback() {
+            @Override
+            public void onInputBufferAvailable(@NonNull MediaCodec codec, int index) {
+                ByteBuffer inputBuffer = codec.getInputBuffer(index);
+                if (inputBuffer == null) {
+                    return;
+                }
+                int size = mExtractor.readSampleData(inputBuffer, 0);
+                if (size < 0) {
+                    return;
+                }
+                long time = size > 0 ? mExtractor.getSampleTime() : 0;
+                int flags = size > 0 ? 0 : MediaCodec.BUFFER_FLAG_END_OF_STREAM;
+
+                codec.queueInputBuffer(index, 0, size, time, flags);
+                mExtractor.advance();
+            }
+
+            @Override
+            public void onOutputBufferAvailable(@NonNull MediaCodec codec, int index, @NonNull MediaCodec.BufferInfo info) {
+                ByteBuffer outputBuffer = mCodec.getOutputBuffer(index);
+                if (outputBuffer == null) {
+                    return;
+                }
+
+                int bufferSizeInBytes = info.size;
+                int desiredDoubleArraySize = bufferSizeInBytes / 4;
+                if (desiredDoubleArraySize > doubleSamples.length) {
+                    doubleSamples = new double[desiredDoubleArraySize];
+                    shortArray = new short[desiredDoubleArraySize*2];
+                    floatFiltered = new float[desiredDoubleArraySize];
+                }
+
+                outputBuffer.order(ByteOrder.LITTLE_ENDIAN).asShortBuffer().get(shortArray, 0, desiredDoubleArraySize*2);
+
+                for (int t = 0; t < desiredDoubleArraySize; t++) {
+                    doubleSamples[t] = ((double) shortArray[2*t]) / 0x8000;
+                }
+
+                filter.process(doubleSamples, floatFiltered, desiredDoubleArraySize);
+//                filter.process(doubleSamples, floatFiltered, desiredDoubleArraySize);
+//                filter.process(doubleSamples, floatFiltered, desiredDoubleArraySize);
+//                filter.process(doubleSamples, floatFiltered, desiredDoubleArraySize);
+//                filter.process(doubleSamples, floatFiltered, desiredDoubleArraySize);
+//                filter.process(doubleSamples, floatFiltered, desiredDoubleArraySize);
+//                filter.process(doubleSamples, floatFiltered, desiredDoubleArraySize);
+//                filter.process(doubleSamples, floatFiltered, desiredDoubleArraySize);
+//                filter.process(doubleSamples, floatFiltered, desiredDoubleArraySize);
+//                filter.process(doubleSamples, floatFiltered, desiredDoubleArraySize);
+
+                // Importante ser blocking ou vai ter que usar FIFO
+                mAt.write(floatFiltered, 0, desiredDoubleArraySize, AudioTrack.WRITE_BLOCKING);
+                mCodec.releaseOutputBuffer(index, false);
+            }
+
+            @Override
+            public void onError(@NonNull MediaCodec codec, @NonNull MediaCodec.CodecException e) {
+
+            }
+
+            @Override
+            public void onOutputFormatChanged(@NonNull MediaCodec codec, @NonNull MediaFormat format) {
+
+            }
+        });
+
+        return;
     }
 
     public void start () {
-        if (mFe.start() == null) {
-            Log.e("Player", "Failed to start fileExtractor, try again later");
-            return;
-        }
-        Player thisPlayer = this;
-        mThread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                thisPlayer.run ();
-            }
-        });
-        mThread.start();
-    }
-
-    private void run () {
-        for (boolean iterate = true; iterate == true;) {
-            ByteBuffer bbuf = mFe.dequeueOutputBuffer();
-            if (bbuf == null) {
-                if (mFe.finishedExtracting) {
-                    iterate = false;
-                }
-                continue;
-            }
-
-            int bufferSizeInBytes = bbuf.limit() - bbuf.position();
-            int desiredFloatArraySize = bufferSizeInBytes / 2;
-            if (desiredFloatArraySize > floatSamples.length) {
-//                floatSamples = new float[desiredFloatArraySize];
-                shortArray = new short[desiredFloatArraySize];
-                doubleSamples = new double[desiredFloatArraySize];
-                floatFiltered = new float[desiredFloatArraySize];
-                doubleFiltered = new double[desiredFloatArraySize];
-            }
-//            for (int i = 0; bbuf.limit() - bbuf.position() > 0; i++) {
-//                Short sample = bbuf.getShort();
-//                Float sampleFloat = (float) sample / 0x8000;
-            bbuf.order(ByteOrder.LITTLE_ENDIAN).asShortBuffer().get(shortArray, 0, desiredFloatArraySize);
-
-            for (int t = 0; t < desiredFloatArraySize; t++) {
-                doubleSamples[t] = ((double) shortArray[t]) / 0x8000;
-            }
-
-
-            filter.process(doubleSamples, floatFiltered, desiredFloatArraySize);
-
-            // NÃO usar floatSamples.length!!!!!
-            // bufferSizeInBytes/2 pode ser menor que floatSamples.length
-//            for (int l = 0; l < bufferSizeInBytes / 2; l++) {
-//                float aux = 0;
-//                for (int j = 0; j < 19; j++) {
-//                    x[j] = x[j + 1];
-//                }
-//                x[19] = floatSamples[l];
-//                aux = 0.0f;
-//                for (int k = 0; k < 20; k++) {
-//                    aux += h[k] * x[19 - k];
-//                }
-//                floatFiltered[l] = aux;
-//            }
-
-            at.write(floatFiltered, 0, bufferSizeInBytes / 2, AudioTrack.WRITE_BLOCKING);
-            mFe.releaseOutputBuffer();
-
-//            ShortBuffer sbuf = bbuf.order(ByteOrder.LITTLE_ENDIAN).asShortBuffer();
-//
-//            short[] audioShorts = new short[sbuf.capacity()];
-//            sbuf.get(audioShorts);
-//
-//            float[] audioFloats = new float[audioShorts.length];
-//            for (int t = 0; t < audioShorts.length; t++) {
-//                audioFloats[t] = ((float)audioShorts[t])/0x8000;
-//            }
-//
-//            //filter.process(audioFloats,float_amp);
-//
-//            Log.i("MyAndroidClass1", String.valueOf(audioFloats.length));
-//            Log.i("MyAndroidClass2", Byte.toString(s_amp[10]));
-////
-//
-//            at.write(audioFloats, 0, 256, AudioTrack.WRITE_BLOCKING);
-        }
-        filter = null;
-        at.stop();
-        at.release();
+        mAt.play();
+        mCodec.start();
     }
 }
